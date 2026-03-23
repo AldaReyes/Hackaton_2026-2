1672dc3ba460c080ee8a0f1d4b2af66a81556d9a
HackHarmony <aldareyes@outlook.com>
2026-03-23 12:16:50 -0600

    Initial Commit

---
+++ AccesAI/AccesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesAIApp.swift
+//  AccesAI
+//
+//  Created by DEVELOP02 on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AccesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccesAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccesAI
+//
+//  Created by DEVELOP02 on 23/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
